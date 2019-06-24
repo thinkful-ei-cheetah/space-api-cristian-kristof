7017c1aeea806c71ae2c43c661cd300ebe2e6646
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-06-24 17:07:17 -0500

    added strict to see

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
+'use strict';
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL
-    || 'postgresql://cristian@localhost/spaced-repetition',
+    || 'postgresql://spaceuser:password@localhost/spaced-repitition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
