2e684abfae28e84db64aa5a99d6f5650bb8ffa7a
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-24 08:06:26 -0700

    day 1 set up

---
+++ seeds/seed.tables.sql
@@ -0,0 +1,44 @@
+BEGIN;
+
+TRUNCATE
+  "word",
+  "language",
+  "user";
+
+INSERT INTO "user" ("id", "username", "name", "password")
+VALUES
+  (
+    1,
+    'admin',
+    'Dunder Mifflin Admin',
+    -- password = "pass"
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+  );
+
+INSERT INTO "language" ("id", "name", "user_id")
+VALUES
+  (1, 'Spanish', 1);
+
+INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
+VALUES
+  (1, 1, 'hola', 'hello', 2),
+  (2, 1, 'agua', 'water', 3),
+  (3, 1, 'manzana', 'apple', 4),
+  (4, 1, 'pollo', 'chicken', 5),
+  (5, 1, 'gato', 'cat', 6),
+  (6, 1, 'abuela', 'grandmother', 7),
+  (7, 1, 'reloj', 'clock', 8),
+  (8, 1, 'zapato', 'shoe', 9),
+  (9, 1, 'biblioteca', 'library', 10),
+  (10, 1, 'ingeniero', 'engineer', null);
+
+
+UPDATE "language" SET head = 1 WHERE id = 1;
+
+-- because we explicitly set the id fields
+-- update the sequencer for future automatic id setting
+SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
+SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
+SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+
+COMMIT;
