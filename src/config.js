ec98c1e2fa5ec5c47e786b8498fa8c543092c7d4
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-28 14:40:11 -0700

    cors issue update

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 80,
+  PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://goofy-mayer-40411d.netlify.com',
   DB_URL: process.env.DATABASE_URL
-    || 'postgresql://spaceuser:password@localhost/spaced-repitition',
+    || 'postgresql://cristian@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
