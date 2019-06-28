0fc3a5f9b91ea13588e83dfaf9dcd5d59164748e
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-28 13:27:34 -0700

    update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT || 80,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
   DB_URL: process.env.DATABASE_URL
