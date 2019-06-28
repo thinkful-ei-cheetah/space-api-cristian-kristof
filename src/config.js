66c87979d2f02b8ab93ac2c2b1995a0fc4a0ce1a
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-28 12:21:35 -0700

    updated cors

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
   DB_URL: process.env.DATABASE_URL
     || 'postgresql://spaceuser:password@localhost/spaced-repitition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
