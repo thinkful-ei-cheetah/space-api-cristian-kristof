3cd63d58b9b21a4f9e362baff8bfc3c8d082c238
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-28 08:37:57 -0700

    update

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -74,7 +74,7 @@ const LanguageService = {
         }) 
       
       let currWord = list.head
-      while (currWord.next !== null) {
+      while (currWord) {
         await trx('word')
           .where('id', currWord.val.id)
           .update({
